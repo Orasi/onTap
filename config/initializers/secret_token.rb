f60bc0f7893f73f383e7be324f79955784934042
Mattwhooo <watson.mattc@gmail.com>
2014-06-03 10:36:42 -0400

    ReInitialize Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LunchLearn::Application.config.secret_key_base = 'eb4f39c50908d8d1b06e0d618bd19c270be1790a4538971bf1ca1192fbd91bc89b1d74a571b3b03c031aae7829122d968daee36af8dc11853b63bb3e083ca1cf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LunchLearn::Application.config.session_store :cookie_store, key: '_LunchLearn_session'
