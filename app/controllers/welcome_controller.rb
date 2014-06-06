cbc909b46202daf20719e1dbf678816bacd24484
lew <LGordon2@gmail.com>
2014-06-06 09:24:51 -0400

    Set up controller param filtering. #12899047107606

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,6 @@
 class WelcomeController < ApplicationController
-  def index
-  end
-
-  def login
-  end
-
   def validate
-    if validate_against_ad(params["login"]["username"], params["login"]["password"])
+    if validate_against_ad(login_params["username"], login_params["password"])
       redirect_to :root
     else
       redirect_to :login, flash: {error: "Invalid username or password."}
@@ -18,7 +12,7 @@ class WelcomeController < ApplicationController
   def validate_against_ad(username, password)
     #Do authentication against the AD.
     return false if password.blank?
-    #return true unless Rails.env.production?
+    return true unless Rails.env.production?
 
     ldap = Net::LDAP.new :host => '10.238.242.32',
     :port => 389,
@@ -28,5 +22,9 @@ class WelcomeController < ApplicationController
       :password => password
     }
     return ldap.bind
+  end
+
+  def login_params
+    params.require(:login).permit(:username, :password)
   end
 end
