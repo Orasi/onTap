f0a14ae1ac1330e97409f273d226d0c9e7e79995
KevinMunro <Kevin.Munro@orasi.com>
2014-06-24 14:29:29 -0700

    EOD lunchlearn tests

--- test/controllers/lunchlearns_controller_test.rb
+++ test/controllers/lunchlearns_controller_test.rb
@@ -4,4 +4,32 @@ class LunchlearnsControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @lunchone=lunchlearns(:lunchone)
+    @employee=users(:employee)
+    @employeetwo=users(:employeetwo)
+    @host=users(:host)
+    @admin=users(:admin)
+  end
+
+  test "should see lunchlearn info if attendee" do
+    get :show, {id: @lunchone.id}, {current_user_id: @employee.id}
+    assert_select 'h5', 'The following users have registered to attend'
+  end
+
+  test "should not see lunchlearn info if not attendee" do
+    get :show, {id: @lunchone.id}, {current_user_id: @employeetwo.id}
+    assert_select 'h5', false, 'Information is hidden from the user'
+  end
+
+  test "should see lunchlearn info if host" do
+    get :show, {id: @lunchone.id}, {current_user_id: @host.id}
+    assert_select 'h5', 'The following users have registered to attend'
+  end
+
+  test "should see lunchlearn info if admin" do
+    get :show, {id: @lunchone.id}, {current_user_id: @admin.id}
+    assert_select 'h5', 'The following users have registered to attend'
+  end
+
 end
