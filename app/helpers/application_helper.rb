a532b0e837314e159f67869dd516f270b94c6b30
KevinMunro <Kevin.Munro@orasi.com>
2014-07-07 07:22:23 -0700

    Implemented bootstrap flash

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
       User.find(session[:current_user_id]).admin
     end
   end
+
+  def bootstrap_class_for flash_type
+    { success: "alert-success", error: "alert-danger", alert: "alert-warning"}[flash_type] || flash_type.to_s
+  end
 end
