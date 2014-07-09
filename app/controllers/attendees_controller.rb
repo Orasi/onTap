37e2f06f92c87041bc5f5a80e557cb9c562cd124
KevinMunro <Kevin.Munro@orasi.com>
2014-07-09 10:02:36 -0700

    Attendee hack fix

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -2,15 +2,17 @@ class AttendeesController < ApplicationController
   def change 
     if Lunchlearn.find(params[:id]).attendees.exists?(user_id: session[:current_user_id])
       Lunchlearn.find(params[:id]).attendees.find_by(user_id: session[:current_user_id]).destroy
-
       flash[:error] = "You are no longer attending the event: #{Lunchlearn.find(params[:id]).title}!"
       redirect_to (:back)
     else
-      Lunchlearn.find(params[:id]).attendees.create(user_id: session[:current_user_id])
-      flash[:success] = "You are now attending the event: #{Lunchlearn.find(params[:id]).title}!"
-       redirect_to (:back)
-
-    end
-   
+      @attendee=Lunchlearn.find(params[:id]).attendees.new(user_id: session[:current_user_id])
+      if @attendee.save
+        flash[:success] = "You are now attending the event: #{Lunchlearn.find(params[:id]).title}!"
+        redirect_to (:back)
+      else
+        flash[:error] = "#{Lunchlearn.find(params[:id]).title} is in the archive."
+        redirect_to :calendar
+      end
+    end   
   end
 end
--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,4 +1,13 @@
 class Attendee < ActiveRecord::Base
   belongs_to :user
   belongs_to :lunchlearn
+  validate :not_in_archive
+
+  def not_in_archive
+    if lunchlearn.lunch_date.past?
+      if !user.check_if_admin?
+        errors.add(:lunch_date, "#{lunchlearn.title} is already in the ")
+      end
+    end
+  end
 end
