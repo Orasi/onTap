f63c9e62b0f97657b3fc1e36d115e249ee74db25
Matt Watson <Matt.Watson@orasi.com>
2014-07-09 08:36:06 -0400

    small changes

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -3,11 +3,11 @@ class AttendeesController < ApplicationController
     if Lunchlearn.find(params[:id]).attendees.exists?(user_id: session[:current_user_id])
       Lunchlearn.find(params[:id]).attendees.find_by(user_id: session[:current_user_id]).destroy
       flash[:error] = 'You are no longer attending the Event!'
-      redirect_to controller: :lunchlearns, action: :calendar
+      redirect_to (:back)
     else
       Lunchlearn.find(params[:id]).attendees.create(user_id: session[:current_user_id])
       flash[:success] = 'You are now attending the Event!'
-      redirect_to controller: :lunchlearns, action: :calendar
+      redirect_to (:back)
     end
    
   end
