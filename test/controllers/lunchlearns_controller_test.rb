01eef1e7260a53e71a1710120e76a0d17f5dcbe7
Matt Watson <Matt.Watson@orasi.com>
2014-07-11 19:48:50 -0400

    update test

--- test/controllers/lunchlearns_controller_test.rb
+++ test/controllers/lunchlearns_controller_test.rb
@@ -15,7 +15,7 @@ class LunchlearnsControllerTest < ActionController::TestCase
 
   test "should see lunchlearn info if attendee" do
     get :show, {id: @lunchone.id}, {current_user_id: @employee.id}
-    assert_select 'h5', 'The following users have registered to attend'
+    assert_select 'h4', 'The following users have registered to attend'
   end
 
   test "should not see lunchlearn info if not attendee" do
@@ -24,18 +24,18 @@ class LunchlearnsControllerTest < ActionController::TestCase
   end
 
   test "should see lunchlearn info if host" do
-    get :show, {id: @lunchone.id}, {current_user_id: @host.id}
-    assert_select 'h5', 'The following users have registered to attend'
+     get :show, {id: @lunchone.id}, {current_user_id: @host.id}
+    assert_select 'h4', 'The following users have registered to attend'
   end
 
   test "should see lunchlearn info if admin" do
     get :show, {id: @lunchone.id}, {current_user_id: @admin.id}
-    assert_select 'h5', 'The following users have registered to attend'
+    assert_select 'h4', 'The following users have registered to attend'
   end
 
   test "host should be listed on show page" do
     get :show, {id: @lunchone.id}, {current_user_id: @employee.id}
-    assert_select 'h1', /.*hosted by Lewis Gordon/
+    assert_select '.h1', /.*Lewis Gordon/
   end
 
   test "if no attendees, no users registered should display" do
