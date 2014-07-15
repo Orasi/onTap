727a13429c533cfd756ec6f769f2675d14e0c29c
Matt Watson <Matt.Watson@orasi.com>
2014-07-14 22:09:35 -0400

    testing seeds

--- db/seeds/staging.rb
+++ db/seeds/staging.rb
@@ -1,2 +1,4 @@
-User.create(username: "company.admin", first_name: "company", last_name: "admin", admin: true)
-User.create((username: "Matt.Watson", first_name: "Matt", last_name: "Watson", admin: true)
+#User.create(username: "company.admin", first_name: "company", last_name: "admin", admin: true)
+#User.create((username: "Matt.Watson", first_name: "Matt", last_name: "Watson", admin: true)
+Lunchlearn.create( title: "sometest", description: "This is some test", lunch_date: "2014-07-15", lunch_time: "2000-01-01 02:00:00", meeting_phone_number: "", access_code: "", has_GoToMeeting: false, go_to_meeting_url: "", end_time: "2000-01-01 08:00:00")
+
