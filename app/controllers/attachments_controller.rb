73fde0a3536d0557d2d22621bfc06cba1c50f8cc
Matt Watson <Matt.Watson@orasi.com>
2014-07-15 09:57:49 -0400

    finishing attachment support

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -2,13 +2,20 @@ class AttachmentsController < ApplicationController
   def create
     @attachment = Attachment.new( attach_params )
     if @attachment.save
-      flash[:success] = "Attachment Added to Event"
+      flash[:success] = "Attachment Added to Event."
     else
-      flash[:error] = "Attachment Failed to be added to Event"
+      flash[:error] = "Attachment Failed to be added to event.  Errors:  " + @attachment.errors.full_messages.to_s
     end
     redirect_to (:back)
   end
 
+  def destroy
+    @attachment = Attachment.find(params[:id])
+    title = @attachment.file_file_name
+    @attachment.destroy
+    flash[:error] = title + " deleted from event"
+    redirect_to (:back)
+  end
   private
 
   def attach_params
--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,4 +1,5 @@
 class Attachment < ActiveRecord::Base
+  validates :file_file_name, presence: true
   belongs_to :lunchlearn
   has_attached_file :file
   validates_attachment_content_type :file, :content_type => /\A.*\Z/
