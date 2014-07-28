42a4990f4d4629f8d5f8ef81517335732e816883
KevinMunro <Kevin.Munro@orasi.com>
2014-07-28 14:28:52 -0700

    migration fix

--- db/migrate/2014071600484932_change_lunch_id_in_host.rb
+++
@@ -1,5 +0,0 @@
-class ChangeLunchIdInHost < ActiveRecord::Migration
-  def change
-    rename_column :hosts, :lunchlearn_id, :event_id
-  end
-end
--- db/migrate/2014071600485932_change_lunch_id_in_attendee.rb
+++
@@ -1,5 +0,0 @@
-class ChangeLunchIdInAttendee < ActiveRecord::Migration
-  def change
-    rename_column :attendees, :lunchlearn_id, :event_id
-  end
-end
