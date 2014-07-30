5fd55ba373627584cacfd880f2a01867dc93f0d9
KevinMunro <Kevin.Munro@orasi.com>
2014-07-30 14:46:27 -0700

    achive and calendar sort fix

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -2,9 +2,8 @@ class ArchiveController < ApplicationController
   before_action :require_admin, only: [:update, :destroy]
 
   def index
- #   @events = Event.includes(Schedule)
-#    @events = Event.where(Event.schedules.first.lunch_date < DateTime.now.to_date).order(lunch_date: :desc)
-     @events = Event.all
+     @events=Event.joins(:schedules).merge(Schedule.where("event_date < ?",DateTime.now.to_date))
+     @events.sort! { |a,b| a.schedules.first.event_date <=> b.schedules.first.event_date }
   end
   
   def show
