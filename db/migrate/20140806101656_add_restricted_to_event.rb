ade5a0390be4fb21578792f7c127397c3c5ecd13
KevinMunro <Kevin.Munro@orasi.com>
2014-08-06 05:27:07 -0700

    fixed mergre

--- db/migrate/20140806101656_add_restricted_to_event.rb
+++
@@ -1,5 +0,0 @@
-class AddRestrictedToEvent < ActiveRecord::Migration
-  def change
-    add_column :events, :restricted, :boolean
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2014072319234223224324) do
+ActiveRecord::Schema.define(version: 201407231923422322432) do
 
   create_table "attachments", force: true do |t|
     t.string   "title"
