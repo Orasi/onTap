17ab01b12d6e62a491983114f20193116ccb812c
KevinMunro <Kevin.Munro@orasi.com>
2014-08-06 14:50:32 -0700

    home commit

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,7 +3,8 @@ module EventsHelper
   def get_requests_for_user
     @requests=Requests.where(user_id: session[:current_user_id])
     @requests.each do |request|
-    flash.now 
+    flash.now[:success] = "something"
+    end
   end
 
   def check_for_requests
