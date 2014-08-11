5cdffc98d4f0ceb23431e01762fd0407e37b1d61
KevinMunro <Kevin.Munro@orasi.com>
2014-08-11 08:43:27 -0700

    Empty Test methods

--- test/controllers/attendees_controller_test.rb
+++ test/controllers/attendees_controller_test.rb
@@ -4,4 +4,13 @@ class AttendeesControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Admin should be added to attendees if event is restricted"
+  end
+
+  test "Base user should generate request when asking to attend a restricted event"
+  end
+
+  test "Base user should destroy request if cancel request was selected"
+  end
 end
