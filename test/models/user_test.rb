cb0e84c326d25b0600bf03ac96ee68231a2450d7
Matt Watson <Matt.Watson@orasi.com>
2014-08-27 16:17:48 -0400

    even more test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -25,7 +25,22 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test 'find or create - find' do 
+    u = User.find_or_create(User.first.username)
+    assert_not_nil u
+  end
+
+  test 'find or create - create' do
+    u = User.find_or_create("some.one")
+    assert_not_nil u
+  end
+
   test 'employee can not login without password' do
     assert_not @user.validate_against_ad('')
   end
+
+  test 'employee can login with password' do
+    assert @user.validate_against_ad("1234")
+  end
+
 end
