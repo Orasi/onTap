dc0e5ea37d5b0a0c030b78fa5d906dcde70b1d37
Matt Watson <Matt.Watson@orasi.com>
2014-08-27 16:08:57 -0400

    1

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+    def setup
+    @lunchlearn = FactoryGirl.create(:lunchlearnstyle)
+    assert @lunchlearn
+    @webinar = FactoryGirl.create(:webinarstyle)
+    assert @webinar
+    @user = FactoryGirl.create(:normal_user)
+    assert @user
+    @admin = FactoryGirl.create(:admin_user)
+    assert @admin
+  end
+
+  test 'two users should not have same username' do
+    e1 = User.new
+    e2 = User.new
+    e1.username = 'john.doe'
+    e1.email = 'john.doe@orasi.com'
+    e1.save
+    e2.username = 'john.doe'
+    e2.email = 'john.doe@orasi.com'
+    assert_raises ActiveRecord::RecordInvalid do
+      e2.save!
+    end
+  end
 end
