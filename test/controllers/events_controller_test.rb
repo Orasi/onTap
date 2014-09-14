f0f2ec6644f740593218e70abcb06d8bd6c81913
Matt Watson <Matt.Watson@orasi.com>
2014-09-14 10:24:13 -0400

    more test

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -22,7 +22,6 @@ class EventsControllerTest < ActionController::TestCase
   end
 
   test 'should get calendar if user logged on' do
-    skip
     get :calendar, {}, current_user_id: @user.id
     assert_response :success
   end
