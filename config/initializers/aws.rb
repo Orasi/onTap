1bdfb9afe3aa41c124ad06ea85f2ca425ad4ed7f
Matt Watson <Matt.Watson@orasi.com>
2014-09-19 13:52:24 -0400

    add aws

---
+++ config/initializers/aws.rb
@@ -0,0 +1,24 @@
+# config/initializers/aws.rb
+require 'aws-sdk'
+# Rails.configuration.aws is used by AWS, Paperclip, and S3DirectUpload
+Rails.configuration.aws = YAML.load(ERB.new(File.read("#{Rails.root}/config/aws.yml")).result)[Rails.env].symbolize_keys!
+AWS.config(logger: Rails.logger)
+AWS.config(Rails.configuration.aws)
+ 
+# config/initializers/paperclip.rb
+Paperclip::Attachment.default_options.merge!(
+  url:                  ':s3_domain_url',
+  path:                 ':class/:attachment/:id/:style/:filename',
+  storage:              :s3,
+  s3_credentials:       Rails.configuration.aws,
+  s3_permissions:       :private,
+  s3_protocol:          'https'
+)
+ 
+#config/initializers/s3_direct_upload.rb
+S3DirectUpload.config do |c|
+  c.access_key_id     = Rails.configuration.aws[:access_key_id]
+  c.secret_access_key = Rails.configuration.aws[:secret_access_key]
+  c.bucket            = Rails.configuration.aws[:bucket]
+  c.region            = "s3"
+end
