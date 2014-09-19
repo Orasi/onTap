aceb8d3a4db21a5b832f14aed0832f92deeb30cf
KevinMunro <Kevin.munro@orasi.com>
2014-09-18 20:23:15 -0700

    more test

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class NotificationsControllerTest < ActionController::TestCase
   def setup
     @user = FactoryGirl.create(:normal_user)
+    @usertwo = FactoryGirl.create(:normal_user)
     @admin = FactoryGirl.create(:admin_user)
     @newnotification = FactoryGirl.create(:notification, :new, :attendancenotification)
     @acceptednotification = FactoryGirl.create(:notification, :approved, :attendancenotification)
@@ -28,16 +29,28 @@ class NotificationsControllerTest < ActionController::TestCase
     assert Notification.where(user_id: @user.id, event_id: eventid, status: "rejected").blank?
   end
 
-  test 'User should not be able to delete another users notifications'
+  test 'User should not be able to delete another users notifications' do
+    @acceptednotification.update_attribute(:user_id, @user.id)
+    eventid=@acceptednotification.event_id
+    assert_not Notification.where(user_id: @user.id, event_id: eventid, status: "approved").blank?
+    delete :destroy, { id: @acceptednotification.id }, current_user_id: @usertwo.id
+    assert_not_nil flash[:error]
+    assert_equal flash[:error], 'Must be notification owner or admin to remove notification'  
   end
 
-  test 'Admin should be able to delete another users request'
+  test 'Admin should be able to delete another users request' do
+    @acceptednotification.update_attribute(:user_id, @user.id)
+    eventid=@acceptednotification.event_id
+    assert_not Notification.where(user_id: @user.id, event_id: eventid, status: "approved").blank?
+    delete :destroy, { id: @acceptednotification.id }, current_user_id: @admin.id
+    assert Notification.where(user_id: @user.id, event_id: eventid, status: "approved").blank?
   end
 
-  test 'Survey notification should not be removable if survey not complete'
+  test 'Survey notification should not be removable if survey not complete' do
   end
 
-
+  test 'Survey notification should be removable if survey is complete' do
+  end
 
 #  test 'Users should see survey notifications' do
 
