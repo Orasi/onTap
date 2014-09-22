103fa04b0073c745d877f6c3dd85f2febfcc5bbc
Matt Watson <Matt.Watson@orasi.com>
2014-09-22 15:13:40 -0400

    aws final test

--- app/assets/javascripts/attachments.js
+++ app/assets/javascripts/attachments.js
@@ -2,14 +2,22 @@
 $(function() {
   $('#s3_uploader').S3Uploader(
     { 
-      remove_completed_progress_bar: true,
+      remove_completed_progress_bar: false,
       progress_bar_target: $('#uploads_container')
     }
   );
   $('#s3_uploader').bind('s3_upload_failed', function(e, content) {
-    return alert(content.filename + ' failed to upload');
+    $('#upload_' + content.unique_id).find('.progress').fadeOut('slow', function(){
+      $('#upload_' + content.unique_id).append('<span style="color:red" class="glyphicon glyphicon-remove-sign"></span> File Failed to Upload. Please Contact System Administrator')
+    });
   });
   $('#s3_uploader').bind('s3_upload_complete', function(e, content) {
-    return alert(content.filename + ' worked');
+    $('#upload_' + content.unique_id).find('.progress').fadeOut('slow', function(){
+      $('#upload_' + content.unique_id).append('<span style="color:green" class="glyphicon glyphicon-ok-sign"></span>')
+    });
+
+  });
+$('#s3_uploader').bind ("ajax:success", function(e, data){
+
   });
 });
--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -4,6 +4,6 @@ class ArchiveController < ApplicationController
   def index
     # @events = Event.joins(:schedules).merge(Schedule.where('event_date < ?', DateTime.now.to_date))
     @events = Event.where(status: :finalized)
-    @events.sort! { |a, b| a.schedules.first.event_date <=> b.schedules.first.event_date }
+    @events.sort! { |b, a| a.schedules.first.event_date <=> b.schedules.first.event_date }
   end
 end
