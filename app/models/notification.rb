0ca352a34b444eb1bbe154efa60dc0855fad7c15
KevinMunro <Kevin.Munro@orasi.com>
2014-09-25 14:26:19 -0700

    start work

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,14 @@ class Notification < ActiveRecord::Base
   belongs_to :event
   validates_inclusion_of :status, in: %w(new rejected approved)
   validates_inclusion_of :notification_type, in: %w(attendance survey)
-end
+
+  def self.notification_cleanup()
+    @events=Event.all
+    @events.each do |event|
+      for 
+        if DateTime.now.to_date - 7 >= Event.find(event_id).schedules.last.event_date.to_date
+        Notification.destroy_all(:event_id==eventid)
+        end
+      end
+    end
+end 
