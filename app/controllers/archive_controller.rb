cd2ae24cbf62b4a1493fbf9d3e78b3f140233fbc
Matt Watson <Matt.Watson@orasi.com>
2014-10-21 08:35:08 -0400

    quick fix to archive

--- app/controllers/archive_controller.rb
+++ app/controllers/archive_controller.rb
@@ -4,6 +4,6 @@ class ArchiveController < ApplicationController
   def index
     # @events = Event.joins(:schedules).merge(Schedule.where('event_date < ?', DateTime.now.to_date))
     @events = Event.where("status = ? OR status = ?", 'finalized', 'closed')
-    @events.sort! { |b, a| a.schedules.first.event_date <=> b.schedules.first.event_date }
+    @events.sort! { |b, a| a.schedules.first.end <=> b.schedules.first.end }
   end
 end
