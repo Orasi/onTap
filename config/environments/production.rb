543247d2bdf05f69fb7d5c4da0a3b13dc5e34a7d
Matt Watson <Matt.Watson@orasi.com>
2014-12-04 10:00:32 -0500

    working on asset serving

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ LunchLearn::Application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
@@ -20,7 +20,7 @@ LunchLearn::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
