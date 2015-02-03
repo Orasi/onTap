6df5133f395fd0c0c6c10186dd3eeca36ad3fdef
KevinMunro <Kevin.Munro@orasi.com>
2015-02-03 11:33:06 -0500

    heroku fix?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,8 @@
 //= require_tree .
 //= require jquery.timepicker.js
 //= require jquery.countdown.js
+//= require lib/abstract-chosen
+//= require lib/select-parser
 //= require chosen-jquery
 //= require browser_timezone_rails/application.js
 
