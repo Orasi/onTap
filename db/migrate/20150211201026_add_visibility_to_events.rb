5603ea7a78e322c018b9e893099a2c574bf00c7c
KevinMunro <Kevin.Munro@orasi.com>
2015-02-11 15:18:33 -0500

    migration magic

--- db/migrate/20150211201026_add_visibility_to_events.rb
+++ db/migrate/20150211201026_add_visibility_to_events.rb
@@ -1,4 +1,4 @@
-class AddFieldsToEvent < ActiveRecord::Migration
+class AddVisibilityToEvents < ActiveRecord::Migration
   def change
     add_column :events, :limited_visibility, :boolean
     add_column :events, :visible_to_departments, :text
