c54dbc7f5b7343805810abe555748ddf0d1bb153
Mattwhooo <Matt.Watson@orasi.com>
2015-03-09 12:15:49 -0400

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -14,7 +14,7 @@ role :db,  %w(deploy@10.238.240.36)
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '10.238.240.36', user: 'deploy', roles: %w(web app), my_property: :my_value
+server 'orasi-portal', user: 'deploy', roles: %w(web app), my_property: :my_value
 
 # Custom SSH Options
 # ==================
