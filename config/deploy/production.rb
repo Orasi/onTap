73292e802026fbc062aac5b7302f40a126c5220b
Matt Watson <Matt.Watson@orasi.com>
2015-03-12 15:48:50 -0400

    update cap

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Supports bulk-adding hosts to roles, the primary server in each group
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
-
+set :rails_env, "production"
 role :web, %w(deploy@orasi-portal)
 
 # Extended Server Syntax
